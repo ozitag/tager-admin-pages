577fd0e158a9208bef6a500de5fdb15ddf683fe0
seryozhabaleyko <seryozha.baleyko@gmail.com>
2021-02-22 19:34:37 +0300

    Add localization

---
+++ src/locales/ru.ts
@@ -0,0 +1,45 @@
+import EN from './en';
+
+const RU: typeof EN = {
+  home: 'Главная',
+  pages: 'Cтраницы',
+  createPage: 'Создать страницу',
+  viewPage: 'Просмотр страницы',
+  pageList: 'Список страниц',
+  pageForm: 'Форма страницы',
+
+  title: 'Название',
+  pageTitle: 'Название страницы',
+  pageDescription: 'Описание страницы',
+  pageUpdate: 'Обновление страницы',
+  pageCreation: 'Создание страницы',
+  openGraphImage: 'Картинка страницы',
+  path: 'Путь',
+  template: 'Шаблон',
+  noTemplate: 'Нет шаблона',
+  actions: 'Действия',
+  edit: 'Редактировать',
+  delete: 'Удалить',
+  AddChildPage: 'Добавить дочернюю страницу',
+  parentPage: 'Родительская страница',
+  noParent: 'Нет родителя',
+  body: 'Тело',
+  image: 'Изображение',
+  excerpt: 'Выдержка',
+
+  tabs: {
+    common: 'Общие',
+    template: 'Шаблон',
+    seo: 'SEO',
+  },
+
+  success: 'Успех',
+  error: 'Ошибка',
+
+  createdSuccessMessage: 'Страница была успешно создана',
+  createdErrorMessage: 'Создание страницы не удалось',
+  updatedSuccessMessage: 'Страница успешно обновлена',
+  updatedErrorMessage: 'Обновление страницы не удалось',
+};
+
+export default RU;
